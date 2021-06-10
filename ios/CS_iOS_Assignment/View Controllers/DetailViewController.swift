fb5c60914ba7a31e4027da0f567a0ce99eb4883f
Gregory Casamento <greg.casamento@gmail.com>
2021-06-10 15:59:51 -0400

    Add Genre list

--- ios/CS_iOS_Assignment/View Controllers/DetailViewController.swift
+++ ios/CS_iOS_Assignment/View Controllers/DetailViewController.swift
@@ -16,9 +16,10 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var releaseDate: UILabel!
     @IBOutlet weak var movieTitle: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var genres: UILabel!
     
     var movieId : Int!
-    // var detailsDictionary : [String : Any] = [:]
+
     var movieService = MovieService.sharedMovieService
     
     override func viewDidLoad() {
@@ -29,11 +30,23 @@ class DetailViewController: UIViewController {
                 let baseUrl : String = self.movieService.baseUrl()
                 let posterPath : String = (jsonDict["poster_path"]) as! String
                 let imagePath : String = "\(baseUrl)w300\(posterPath)"
+                
                 self.movieService.fetchImage(view: self.imageView, path: imagePath)
                 self.movieTitle.text = (jsonDict["original_title"]) as? String
                 self.releaseDate.text = (jsonDict["release_date"]) as? String
                 self.runTime.text = (jsonDict["runtime"]) as? String
                 self.overviewText.text = (jsonDict["overview"]) as? String
+                
+                var genreString = "Genres:"
+                let genreArray = (jsonDict["genres"]) as? [Any]
+                genreArray?.forEach({ g in
+                    let genreDict = g as? [String : Any]
+                    let s = genreDict?["name"] as? String
+                    genreString.append(" ")
+                    genreString.append(s!)
+                })
+                
+                self.genres.text = genreString
             }
         }
     }
--- ios/CS_iOS_AssignmentTests/CS_iOS_AssignmentTests.swift
+++ ios/CS_iOS_AssignmentTests/CS_iOS_AssignmentTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 class CS_iOS_AssignmentTests: XCTestCase {
 
+    let movieService = MovieService.sharedMovieService
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -19,16 +21,13 @@ class CS_iOS_AssignmentTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testMostPopularMovieList() {
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testNowPlayingMovieList() {
+    }
+
+    func testConfiguration() {
     }
 
 }
